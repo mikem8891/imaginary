a3e364d4c930eb2f26b70c168a9b85d11da1a658
Mike Maley <michael.r.maley@gmail.com>
2023-09-02 19:39:43 -0400

    update src/c32.rs

--- src/c32.rs
+++ src/c32.rs
@@ -2,6 +2,8 @@ use super::*;
 
 pub const I: Complex<f32> = Complex{r: 0.0_f32, i: 1.0_f32};
 
+impl_complex_float_ops!(f32);
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +11,12 @@ mod tests {
     #[test]
     fn check_const_path(){
         assert_eq!(crate::c32::I, Complex{r: 0.0_f32, i: 1.0_f32});
+    }
+    
+    #[test]
+    fn check_complex_float_ops(){
+        let a = 1.0_f32;
+        let b = Complex::from((2.0_f32, 3.0_f32));
+        assert_eq!(a + b, Complex{r: 3.0_f32, i: 3.0_f32});
     }
 }
