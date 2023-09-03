64e89e581edaacd3584ad997d9124fbf284d9648
Mike Maley <michael.r.maley@gmail.com>
2023-09-03 10:17:59 -0400

    update src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -133,6 +133,7 @@ mod tests {
     fn basic_ops() {
         let a = Complex::from((1.0, 2.0));
         assert_eq!(a, Complex{r: 1.0, i: 2.0});
+        assert_eq!(a.into(), (1.0, 2.0));
         assert_eq!(-a, Complex{r: -1.0, i: -2.0});
         assert_eq!(a+a, Complex{r: 2.0, i: 4.0});
         assert_eq!(a-a, Complex{r: 0.0, i: 0.0});
