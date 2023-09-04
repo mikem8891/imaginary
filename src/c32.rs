162b96a602422f3aedf329fea22614adc6afd96a
Mike Maley <michael.r.maley@gmail.com>
2023-09-04 18:22:19 -0400

    update src/c32.rs

--- src/c32.rs
+++ src/c32.rs
@@ -5,6 +5,7 @@ pub const I: Complex<f32> = Complex{r: 0.0_f32, i: 1.0_f32};
 impl_complex_float_ops!(f32);
 
 impl Complex<f32> {
+    pub const I = Complex::<f32>::new(0.0, 1.0);
     impl_abs!(f32);
     impl_angle!(f32);
     impl_cis!(f32);
@@ -39,14 +40,13 @@ mod tests {
     
     #[test]
     fn float_fn() {
-        const PI: f32 = std::f32::consts::PI;
-        let a = Complex::<f32>::new(3.0, -4.0);
+        let a = Complex<f32>::new(3.0, -4.0);
         assert_eq!(a.abs(), 5.0);
-        let e_pi_i = Complex::new(0.0, PI).exp();
-        assert!((e_pi_i + Complex::new(1.0, 0.0)).abs() <= f32::EPSILON);
+        let e_pi_i = Complex::new(0.0_f32, std::f32::consts::PI).exp();
+        assert!((e_pi_i + Complex::new(1.0_f32, 0.0_f32)).abs() <= f32::EPSILON);
         let a_sqrt = a.sqrt();
         assert_eq!(a_sqrt * a_sqrt, a);
-        assert_eq!(Complex::new( 4.0, 0.0).sqrt(), Complex::new(2.0, 0.0));
-        assert_eq!(Complex::new(-4.0, 0.0).sqrt(), Complex::new(0.0, 2.0));
+        assert_eq!(Complex::new( 4.0_f32, 0.0_f32).sqrt(), Complex::new(2.0_f32, 0.0_f32));
+        assert_eq!(Complex::new(-4.0_f32, 0.0_f32).sqrt(), Complex::new(0.0_f32, 2.0_f32));
     }
 }
