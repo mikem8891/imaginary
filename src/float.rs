96e7759f1d5118bf82bcce8f5cd558f1e9232376
Mike Maley <michael.r.maley@gmail.com>
2023-09-05 22:58:32 -0400

    update src/float.rs

--- src/float.rs
+++ src/float.rs
@@ -230,7 +230,14 @@ macro_rules! impl_complex_fn {
             pub fn powc(self, n: Complex<$t>) -> Complex<$t> {
                 (n * self.ln()).exp()
             }
-
+            /// Square root, √x
+            pub fn sqrt(x: f32) -> Complex<$t> {
+                if x >= 0.0 {
+                    Complex::new(x.sqrt(), 0.0)
+                } else {
+                    Complex::new(0.0, (-x).sqrt())
+                }
+            }
             /// Square root, √z
             pub fn sqrt(self) -> Complex<$t> {
                 let (x, y) = self.into();
